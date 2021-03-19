932b6f16856f15b4e3c8d1f124732624e3d96860
burhon94 <tohirov1994@gmail.com>
2021-03-19 12:19:53 +0500

    feat: var massages & print(massages)

---
+++ server.py
@@ -0,0 +1,21 @@
+from pprint import pprint
+
+massages = [
+    {
+        'name': "Nick",
+        'text': "Hello, world",
+        'time': "19/Mar 12:07:25"
+    },
+    {
+        'name': "Marry",
+        'text': "Hello Nick!, How are you?",
+        'time': "19/Mar 12:07:29"
+    },
+    {
+        'name': "Nick",
+        'text': "Hi Marry, I'm fine, what about you?",
+        'time': "19/Mar 12:08:07"
+    }
+]
+
+pprint(massages)
