92e1471156dd0bed336f8537ddec567dde534186
Burhonjon Tohirov <tohirov1994@gmail.com>
2021-10-07 14:02:00 +0500

    remove conf data

--- db/db.py
+++ db/db.py
@@ -7,7 +7,6 @@ import psycopg2 as psycopg2
 db_url = os.environ.get('DATABASE_URL')
 if db_url is None:
     sys.exit('db env not accept')
-    # db_url = "postgres://esgzabmdfxgbev:4aa3757628bbdb5c6fb149e9e5699985b9f5a2b65ef0f22d0f82de2a52f3eda5@ec2-54-161-239-198.compute-1.amazonaws.com:5432/d7u8mb88fc2j9h"
 
 result = urlparse(db_url)
 username = result.username
